119f05ec6d08ef67db97c8d782522a5f0e98d46d
Valeria Serratos <valeriaserratos@Valerias-MacBook-Air-86.local>
2025-02-20 11:35:27 -0500

    Created methods

--- discussion6.py
+++ discussion6.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-
+import csv
 
 def load_csv(f):
     '''
@@ -17,9 +17,35 @@ def load_csv(f):
 
     base_path = os.path.abspath(os.path.dirname(__file__))
     full_path = os.path.join(base_path, f)
+    nested_dict = {}
+
+    with open(full_path, mode = 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        for row in reader: 
+            if len(row) < 3:
+                continue
+            month, year, value= row[:3]
+            if year not in nested_dict:
+                nested_dict[year] = {}
+            nested_dict[year][month] = value
+        return nested_dict
+
     # use this 'full_path' variable as the file that you open
 
 def get_annual_max(d):
+    annual_max = []
+    for year, months in d.items():
+        max_month = None
+        max_values = float('-inf')
+        for month, value in months.items():
+            int_value = int(value)
+            if int_value > max_values:
+                max_values = int_value
+                max_month = month
+        if max_month is not None:
+            annual_max.append((year, max_month, max_values))
+    return annual_max
     '''
     Params:
         d, dict created by load_csv above
@@ -31,9 +57,15 @@ def get_annual_max(d):
     Note: Don't strip or otherwise modify strings. Do not change datatypes except where necessary.
         You'll have to change vals to int to compare them. 
     '''
-    pass
+    
 
 def get_month_avg(d):
+    avg_dict ={}
+    for year, months in d.items():
+        values = [int(value)for value in months.values()]
+        if values:
+            avg_dict[year]
+    pass
     '''
     Params: 
         d, dict created by load_csv above
@@ -68,6 +100,12 @@ class dis7_test(unittest.TestCase):
 
 def main():
     unittest.main(verbosity=2)
+    print("----------------------------------------------------------------------")
+    flight_dict = load_csv('daily_visitors.csv')
+    print("Output of load_csv:", flight_dict, "\n")
+    print("Output of get_annual_max:", get_annual_max(flight_dict), "\n")
+    print("Output of get_month_avg:", get_month_avg(flight_dict), "\n")
+
 
 if __name__ == '__main__':
     main()
